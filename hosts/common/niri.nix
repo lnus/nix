e935fa04a8481231e38ac4271811c24928ad8b96
Linus <17277861+lnus@users.noreply.github.com>
2025-10-24 17:26:17 +0200

    niri portals, need to fix

--- hosts/common/niri.nix
+++ hosts/common/niri.nix
@@ -6,6 +6,16 @@
     enable = true;
   };
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-gnome
+    ];
+
+    config.common.default = "gnome";
+  };
+
   environment.systemPackages = with pkgs; [
     xwayland-satellite
     playerctl
