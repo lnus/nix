62a20c36057b0bb299db637d7d55f32f455c664f
Linus <17277861+lnus@users.noreply.github.com>
2025-11-16 16:14:56 +0100

    zellij config

--- home/core/cli.nix
+++ home/core/cli.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   home.packages = [
     pkgs.tree
     pkgs.gh # easy auth while working
@@ -9,11 +13,21 @@
     flags = ["--disable-up-arrow"];
   };
 
+  programs.zellij = {
+    enable = true;
+
+    settings = {
+      default_shell = "${lib.getExe pkgs.nushell}";
+      show_startup_tips = false;
+
+      simplified_ui = true;
+    };
+  };
+
   programs.fd.enable = true;
   programs.carapace.enable = true;
   programs.direnv.enable = true;
   programs.yazi.enable = true;
   programs.zoxide.enable = true;
   programs.skim.enable = true;
-  programs.zellij.enable = true; # TODO config
 }
