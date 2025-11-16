470bf55383475645818b328dcbbec7614f76d93f
Linus <17277861+lnus@users.noreply.github.com>
2025-11-16 16:56:11 +0100

    refactor: nushell cleanup

--- home/core/shell.nix
+++ home/core/shell.nix
@@ -1,15 +1,4 @@
-{
-  pkgs,
-  lib,
-  config,
-  inputs,
-  ...
-}: {
-  home.packages = [
-    inputs.conch.packages.${pkgs.stdenv.hostPlatform.system}.default
-  ];
-
-  home.shell.enableNushellIntegration = true;
+{...}: {
   home.shell.enableBashIntegration = true;
 
   home.shellAliases = {
@@ -26,62 +15,5 @@
 
   programs.bash.enable = true;
 
-  programs.nushell = {
-    enable = true;
-
-    extraConfig = ''
-      $env.PROMPT_COMMAND = {|| conch}
-      $env.PROMPT_COMMAND_RIGHT = {||}
-    '';
-
-    settings = {
-      buffer_editor = "hx";
-      edit_mode = "vi";
-      show_banner = false;
-      cursor_shape = {
-        vi_insert = "line";
-        vi_normal = "block";
-      };
-
-      keybindings = [
-        # TODO: write as nushell scripts? writeshellbin?
-        # https://github.com/junegunn/fzf/issues/4122#issuecomment-2607368316
-        {
-          name = "skim_dirs";
-          modifier = "Alt";
-          keycode = "char_c";
-          mode = ["vi_insert" "vi_normal"];
-          event = {
-            send = "executehostcommand";
-            cmd = ''
-              let res = ${lib.getExe pkgs.fd} -t d
-              | ${lib.getExe pkgs.skim} --preview "${lib.getExe pkgs.eza} --tree -L 2 {}";
-              commandline edit --append $res;
-              commandline set-cursor --end
-            '';
-          };
-        }
-        {
-          name = "zoxide_interactive";
-          modifier = "Alt";
-          keycode = "char_z";
-          mode = ["vi_insert" "vi_normal"];
-          event = {
-            send = "executehostcommand";
-            cmd = ''
-              let res = ${lib.getExe pkgs.zoxide} query -i;
-              commandline edit --append $res;
-              commandline set-cursor --end
-            '';
-          };
-        }
-      ];
-    };
-
-    shellAliases =
-      config.home.shellAliases
-      // {
-        fg = "job unfreeze";
-      };
-  };
+  imports = [./nushell];
 }
