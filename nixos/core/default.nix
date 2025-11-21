13b6f26026cdd5a5b27b4fd68ef7387f88ebc280
Linus <17277861+lnus@users.noreply.github.com>
2025-11-21 03:26:35 +0100

    feat(pkgs): nh + nh.clean
    yeah i used 3 conventions
    i think this is the best one though

--- nixos/core/default.nix
+++ nixos/core/default.nix
@@ -6,12 +6,20 @@
   ];
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
+
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
     git
     vim
   ];
+
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/linus/nix"; # TEMP
+  };
 
   system.stateVersion = "25.05";
 }
