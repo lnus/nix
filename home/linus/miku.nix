c8bd19b1cfe8b32cc2f471ff8418675d32862501
Linus <17277861+lnus@users.noreply.github.com>
2026-02-06 15:33:19 +0100

    home: add bolt launcher desktop entry
    miku: add bolt wrapper to spawn launcher under niri

--- home/linus/miku.nix
+++ home/linus/miku.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [
     ./home.nix
     ../features/cli
@@ -60,6 +64,25 @@
     claude-code
     codex
   ];
+
+  # Semi-jank patch to make wrapper behave in Niri
+  # Noctalia calls Quickshell.execDetached(["niri", "msg", "action", "spawn", "--"].concat(command));
+  # TODO Properly fix; probably issue with compositor spawning
+  # TODO Move out to module?
+  xdg.desktopEntries."Bolt" = let
+    bolt-wrap = pkgs.writeShellScriptBin "bolt-wrap" ''
+      sh -c bolt-launcher "$@"
+    '';
+  in {
+    categories = ["Game"];
+    comment = "An alternative launcher for RuneScape";
+    exec = "${lib.getExe bolt-wrap}";
+    genericName = "bolt-launcher";
+    icon = "bolt-launcher";
+    name = "Bolt Launcher";
+    terminal = false;
+    type = "Application";
+  };
 
   home.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/linus/.steam/root/compatibilitytools.d";
