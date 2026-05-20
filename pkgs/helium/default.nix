f15aced4fad646cfca303cc15264411e01952554
Linus <17277861+lnus@users.noreply.github.com>
2026-05-21 01:20:32 +0200

    pkgs: helium 0.10.8.1 -> 0.12.4.1
    [U.] helium 0.10.8.1, 0.10.8.1-bwrap, 0.10.8.1-extracted, 0.10.8.1-fhsenv-profile, 0.10.8.1-fhsenv-rootfs, 0.10.8.1-init -> 0.12.4.1, 0.12.4.1-bwrap, 0.12.4.1-extracted, 0.12.4.1-fhsenv-profile, 0.12.4.1-fhsenv-rootfs, 0.12.4.1-init

--- pkgs/helium/default.nix
+++ pkgs/helium/default.nix
@@ -8,12 +8,12 @@
   fetchurl,
 }: let
   pname = "helium";
-  version = "0.10.8.1";
+  version = "0.12.4.1";
 
   architectures = {
     "x86_64-linux" = {
       arch = "x86_64";
-      hash = "sha256-pN/Iw1ANggDOxxFb2CN436qbcrs8/bDcEqjZC80grQs=";
+      hash = "sha256-OgS8HkLBseFrEhNFJxMwp1bg0gzPdfY1VaySAAp7vq0=";
     };
   };
 
