1869e3f394dcb5d90a17bd925f629702ff46f1e4
Linus <17277861+lnus@users.noreply.github.com>
2026-05-26 18:32:27 +0200

    home/noctalia: remove control center widget

--- home/features/desktop/noctalia.nix
+++ home/features/desktop/noctalia.nix
@@ -36,10 +36,6 @@ in {
 
           widgets = {
             left = [
-              {
-                id = "ControlCenter";
-                useDistroLogo = true;
-              }
               {id = "Workspace";}
               {id = "ActiveWindow";}
               {id = "SystemMonitor";}
