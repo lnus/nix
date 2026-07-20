b6914f31ca71a57dabaccc918327279635ec97bf
Linus <17277861+lnus@users.noreply.github.com>
2026-07-21 01:30:30 +0200

    coreUser: give linus wheel and enable sudo
    was locked out, couldnt run nixos-rebuild

--- modules/system/core/user.nix
+++ modules/system/core/user.nix
@@ -8,9 +8,12 @@
     users.users.linus = {
       initialPassword = "password";
       isNormalUser = true;
+      extraGroups = ["wheel"];
       description = "linus";
       shell = self.packages.${pkgs.stdenv.hostPlatform.system}.nushell;
     };
+
+    security.sudo.enable = true;
 
     environment.variables.EDITOR = "hx";
     environment.variables.VISUAL = "hx";
